0b53040b93ab88a3004e4eddb697173709d141cc
RussBus24 <rjhenry1014@gmail.com>
2016-07-06 14:50:17 -0400

    Street Fighter JS files

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1000px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '500px');
+			});
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+
+$(document).keydown(function(x) {
+	if (x.which == 88) {
+		$('.ryu-ready').hide();
+		$('.ryu-cool').show();
+	}
+})
+	.keyup(function(x) {
+		if (x.which == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-ready').show();
+		}
+	})
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 1;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
