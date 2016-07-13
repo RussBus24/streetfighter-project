62e5cada94015763064e6788483b2e1def80b03a
RussBus24 <rjhenry1014@gmail.com>
2016-07-13 15:48:24 -0400

    App.js updated with correct Ryu standings

--- js/app.js
+++ js/app.js
@@ -27,13 +27,14 @@ $(document).ready(function() {
 $(document).keydown(function(x) {
 	if (x.which == 88) {
 		$('.ryu-ready').hide();
+		$('.ryu-still').hide();
 		$('.ryu-cool').show();
 	}
 })
 	.keyup(function(x) {
 		if (x.which == 88) {
 			$('.ryu-cool').hide();
-			$('.ryu-ready').show();
+			$('.ryu-still').show();
 		}
 	})
 });
